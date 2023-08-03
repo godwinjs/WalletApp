e50ca61433fa66958b22db41f6912fe79a7efdbd
Honeypot <godwinikechukwu.dev@gmail.com>
2023-08-03 12:04:23 -0700

    Update SignUp.js

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState} from "react"
 import {
     View, 
     Text,    
@@ -17,7 +17,237 @@ import LinearGradient from 'react-native-linear-gradient'
 
 import { COLORS, SIZES, FONTS, icons, images } from "../constants"
 
+
+// VariablesConstants
+const imagerl = '../assets/icons/back.png'
+
 const SignUp = () => {
+    // States
+    const [showPassword, setShowPassword] = React.useState(false)
+
+    const [areas, setAreas] = React.useState([])
+    const [selectedArea, setSelectedArea] = React.useState(null)
+    const [modalVisible, setModalVisible] = React.useState(false)
+
+    // Effect
+    React.useEffect(() => {
+    fetch("https://restcountries.eu/rest/v2/all")
+        .then(response => response.json())
+        .then(data => {
+            let areaData = data.map(item => {
+                return {
+                    code: item.alpha2Code,
+                    name: item.name,
+                    callingCode: `+${item.callingCodes[0]}`,
+                    flag: `https://www.countryflags.io/${item.alpha2Code}/flat/64.png`
+                }
+            })
+
+            setAreas(areaData)
+
+            if (areaData.length > 0) {
+                let defaultData = areaData.filter(a => a.code == "US")
+
+                if (defaultData.length > 0) {
+                    setSelectedArea(defaultData[0])
+                }
+            }
+        })
+    }, [])
+
+    // Functions
+    function renderHeader() {
+        return(
+            <TouchableOpacity
+                style={{
+                    flexDirection: 'row',
+                    alignIes: 'center',
+                    marginTop: SIZES.padding * 6,
+                    paddingHorizontal: SIZES.padding * 2
+                }}
+                onPress={() => console.log('Sign Up...')}
+            >
+                <Image 
+                    source={require('../assets/icons/back.png')}
+                    resizeMode="contain"
+                    style={{
+                        width: 20,
+                        height: 20,
+                        tintColor: COLORS.white
+                    }}
+                />{/*inlined*/}
+                <Text style={{ marginLeft: SIZES.padding * 1.5, color: COLORS.white, ...FONTS.h4 }}>Sign Up</Text>
+            </TouchableOpacity>
+        )
+    }
+    function renderLogo() {
+        return (
+            <View
+                style={{
+                    marginTop: SIZES.padding * 5,
+                    height: 100,
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}
+            >
+                <Image
+                    source={require('../assets/images/wallie-logo.png')}
+                    resizeMode="contain"
+                    style={{
+                        width: "60%"
+                    }}
+                />
+            </View>
+        )
+    }
+    function renderForm() {
+        return (
+            <View
+                style={{
+                    marginTop: SIZES.padding * 3,
+                    marginHorizontal: SIZES.padding * 3,
+                }}
+            >
+                {/* Full Name */}
+                <View style={{ marginTop: SIZES.padding * 3 }}>
+                    <Text style={{ color: COLORS.lightGreen, ...FONTS.body3 }}>Full Name</Text>
+                    <TextInput
+                        style={{
+                            marginVertical: SIZES.padding,
+                            borderBottomColor: COLORS.white,
+                            borderBottomWidth: 1,
+                            height: 40,
+                            color: COLORS.white,
+                            ...FONTS.body3
+                        }}
+                        placeholder="Enter Full Name"
+                        placeholderTextColor={COLORS.white}
+                        selectionColor={COLORS.white}
+                    />
+                </View>
+
+                {/* Phone Number */}
+                <View style={{ marginTop: SIZES.padding * 2 }}>
+                    <Text style={{ color: COLORS.lightGreen, ...FONTS.body3 }}>Phone Number</Text>
+
+                    <View style={{ flexDirection: 'row' }}>
+                        {/* Country Code */}
+                        <TouchableOpacity
+                            style={{
+                                width: 100,
+                                height: 50,
+                                marginHorizontal: 5,
+                                borderBottomColor: COLORS.white,
+                                borderBottomWidth: 1,
+                                flexDirection: 'row',
+                                ...FONTS.body2
+                            }}
+                            onPress={() => console.log('show modal')}
+                        >
+                            <View style={{ justifyContent: 'center' }}>
+                                <Image
+                                    source={require('../assets/icons/down.png')}
+                                    style={{
+                                        width: 10,
+                                        height: 10,
+                                        tintColor: COLORS.white
+                                    }}
+                                />
+                            </View>
+                                <View style={{ justifyContent: 'center', marginLeft: 5 }}>
+                                    <Image
+                                        source={{uri: selectedArea?.flag}}
+                                        resizeMode="contain"
+                                        style={{
+                                            width: 30,
+                                            height: 30
+                                        }}
+                                    />
+                                </View>
+
+                            {/* Contry Code */}
+                            <View style={{ justifyContent: 'center', marginLeft: 5 }}>
+                                <Text style={{ color: COLORS.white, ...FONTS.body3 }}>{'US+1'}</Text>
+                            </View>
+                        </TouchableOpacity>
+
+                        {/* Phone Number */}
+                        <TextInput
+                            style={{
+                                flex: 1,
+                                marginVertical: SIZES.padding,
+                                borderBottomColor: COLORS.white,
+                                borderBottomWidth: 1,
+                                height: 40,
+                                color: COLORS.white,
+                                ...FONTS.body3
+                            }}
+                            placeholder="Enter Phone Number"
+                            placeholderTextColor={COLORS.white}
+                            selectionColor={COLORS.white}
+                        />
+                    </View>
+                </View>
+                {/* Password */}
+                <View style={{ marginTop: SIZES.padding * 2 }}>
+                    <Text style={{ color: COLORS.lightGreen, ...FONTS.body3 }}>Password</Text>
+                    <TextInput
+                        style={{
+                            marginVertical: SIZES.padding,
+                            borderBottomColor: COLORS.white,
+                            borderBottomWidth: 1,
+                            height: 40,
+                            color: COLORS.white,
+                            ...FONTS.body3
+                        }}
+                        placeholder="Enter Password"
+                        placeholderTextColor={COLORS.white}
+                        selectionColor={COLORS.white}
+                        secureTextEntry={!showPassword}
+                    />
+                    <TouchableOpacity
+                        style={{
+                            position: 'absolute',
+                            right: 0,
+                            bottom: 10,
+                            height: 30,
+                            width: 30
+                        }}
+                        onPress={() => setShowPassword(!showPassword)}
+                    >
+                        <Image
+                            source={showPassword ? require('../assets/icons/disable_eye.png') : require('../assets/icons/eye.png')}
+                            style={{
+                                height: 20,
+                                width: 20,
+                                tintColor: COLORS.white
+                            }}
+                        />
+                    </TouchableOpacity>
+                </View>
+
+            </View>
+        )
+    }
+    function renderButton() {
+        return (
+            <View style={{ margin: SIZES.padding * 3 }}>
+                <TouchableOpacity
+                    style={{
+                        height: 60,
+                        backgroundColor: COLORS.black,
+                        borderRadius: SIZES.radius / 1.5,
+                        alignItems: 'center',
+                        justifyContent: 'center'
+                    }}
+                    onPress={() => console.log("Home...")}
+                >
+                    <Text style={{ color: COLORS.white, ...FONTS.h3 }}>Continue</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
     return (
         <KeyboardAvoidingView
             behavior={Platform.OS === 'ios' ? 'padding' : null}
@@ -25,11 +255,18 @@ const SignUp = () => {
         >
             <LinearGradient
                 colors={[COLORS.lime, COLORS.emerald]}
+                style={{flex: 1}}
             >
-
+                <ScrollView>
+                    {renderHeader()}
+                    {renderLogo()}
+                    {renderForm()}
+                    {renderButton()}
+                </ScrollView>
             </LinearGradient>
         </KeyboardAvoidingView>
     )
 }
+
 
 export default SignUp;
